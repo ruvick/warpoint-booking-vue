0560c3922a58c57fd6ef5a33b2d65f79d02baaed
Ruvick <rudikov.web@ya.ru>
2025-04-12 19:32:31 +0300

    Сборка

---
+++ dist/assets/use-timeout-22R8yFdA.js
@@ -0,0 +1,1 @@
+import{r as u}from"./QLayout-DVbqn8Rx.js";import{h as i,D as m,t as s}from"./index-Bs82lVcd.js";function f(){let e=null;const o=i();function t(){e!==null&&(clearTimeout(e),e=null)}return m(t),s(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
--- dist/assets/use-timeout-DL8S3auP.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./QLayout-ClYczOob.js";import{h as i,D as m,t as s}from"./index-Esmsc9vZ.js";function f(){let e=null;const o=i();function t(){e!==null&&(clearTimeout(e),e=null)}return m(t),s(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
